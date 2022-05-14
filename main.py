c9243bda6020033436f3e2f276d31103d5a57e1e
Sheikh Fahim Faysal Sowrav <gfahim38@gmail.com>
2022-05-14 20:36:24 +0600

    No Update

---
+++ main.py
@@ -0,0 +1,8 @@
+'''The comfortable relative humidity for humans is between 40% and 60%.
+Write a program that will take input todays humidity in the variable “hum”.
+Then complete the code to output "norm" if the taken percent of humidity is
+in the range of 40 and 60 inclusive. Output “High” when exceeds 60% and
+“Low” when less than 40%. Output “ERROR Input” when the input is wrong.'''
+
+
+
